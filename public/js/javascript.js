344bc76896449f8648a9a616dc8696a5c06d3cf4
Brenton <brentonjjenkins@gmail.com>
2019-08-20 15:26:35 -0700

    lots of work on homepage

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -2,16 +2,23 @@ $(document).ready(function () {
 
     $(function () {
         $(document).scroll(function () {
-            var $nav = $(".navbar");
-            var $navname = $(".nav-name");
-            var $navlink = $(".nav-link");
-            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-            $navname.toggleClass('scrolled', $(this).scrollTop() > $navname.height());
-            $navlink.toggleClass('scrolled', $(this).scrollTop() > $navlink.height());
+            var nav = $(".navbar");
+
+            nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+            nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+            nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+
+            if ($(this).scrollTop() > nav.height()) {
+                nav.removeClass('navbar-dark');
+                nav.addClass('navbar-light');
+            } else {
+                nav.addClass('navbar-dark');
+            }
+
         });
     });
 
-    $('.logo').on("click", function () {
+    $('.logo-footer').on("click", function () {
         $(window).scrollTop(100);
     });
 
